8bca4e59f09e487d0ef565c2cba3c19ab2a25457
delsborg <don.elsborg@gmail.com>
2015-10-02 13:37:33 -0600

    Update usersearchsettings.sql

--- usersearchsettings.sql
+++ usersearchsettings.sql
@@ -1,3 +1,6 @@
+/*
+This script collects all user search settings. Run this before and after an upgrade or patch. Save the file and run checksums and/or diffs to compare results before and after an upgrade.
+*/
 select distinct u.ID, u.[Last Name], u.[First Name], d.Affiliation, o.[Affiliation Override], d.Author, d.Journal, d.Keyword, d.[Start Date] 
 from [User] u, [User Search Term Defaults] d, [User Search Term Overrides] o 
 where 
